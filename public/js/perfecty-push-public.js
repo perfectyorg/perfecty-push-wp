06b960671dfbc4fd10adbb0228174b965137b303
Rowinson Gallego <rwn.gallego@gmail.com>
2020-11-12 00:15:17 +0100

    Implement basic settings dialog

--- public/js/perfecty-push-public.js
+++ public/js/perfecty-push-public.js
@@ -55,9 +55,9 @@
 	}
 	
 	async function drawDialogControl(options) {
-		let dialogControl =
+		const dialogControl =
 		'<div class="perfecty-push-dialog-container" id="perfecty-push-dialog-container">' +
-		'  <div class="perfecty-push-dialog-notify-box">' +
+		'  <div class="perfecty-push-dialog-box">' +
 		'    <div class="perfecty-push-dialog-title">' + options.title + '</div>' +
 		'    <div>' + 
 		'      <button id="perfecty-push-dialog-cancel" type="button" class="secondary">' + options.cancel + '</button>' +
@@ -68,18 +68,66 @@
 		document.body.insertAdjacentHTML('beforeend', dialogControl);
 	}
 
-	async function drawDialogControl() {
-
-	}
-
 	function showDialogControl() {
-		const dialogContainer = document.getElementById('perfecty-push-dialog-container');
-		dialogContainer.style.display = "block";
+		const control = document.getElementById('perfecty-push-dialog-container');
+		control.style.display = "block";
 	}
 
 	function hideDialogControl() {
-		const dialogContainer = document.getElementById('perfecty-push-dialog-container');
-		dialogContainer.style.display = "none";
+		const control = document.getElementById('perfecty-push-dialog-container');
+		control.style.display = "none";
+	}
+
+	async function drawSettingsControl(options) {
+		const svg = 'data:image/svg+xml;base64,PHN2ZyBhcmlhLWhpZGRlbj0idHJ1ZSIgZm9jdXNhYmxlPSJmYWxzZSIgZGF0YS1wcmVmaXg9ImZhcyIgZGF0YS1pY29uPSJiZWxsIiBjbGFzcz0ic3ZnLWlubGluZS0tZmEgZmEtYmVsbCBmYS13LTE0IiByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDQ0OCA1MTIiPjxwYXRoIGZpbGw9ImN1cnJlbnRDb2xvciIgZD0iTTIyNCA1MTJjMzUuMzIgMCA2My45Ny0yOC42NSA2My45Ny02NEgxNjAuMDNjMCAzNS4zNSAyOC42NSA2NCA2My45NyA2NHptMjE1LjM5LTE0OS43MWMtMTkuMzItMjAuNzYtNTUuNDctNTEuOTktNTUuNDctMTU0LjI5IDAtNzcuNy01NC40OC0xMzkuOS0xMjcuOTQtMTU1LjE2VjMyYzAtMTcuNjctMTQuMzItMzItMzEuOTgtMzJzLTMxLjk4IDE0LjMzLTMxLjk4IDMydjIwLjg0QzExOC41NiA2OC4xIDY0LjA4IDEzMC4zIDY0LjA4IDIwOGMwIDEwMi4zLTM2LjE1IDEzMy41My01NS40NyAxNTQuMjktNiA2LjQ1LTguNjYgMTQuMTYtOC42MSAyMS43MS4xMSAxNi40IDEyLjk4IDMyIDMyLjEgMzJoMzgzLjhjMTkuMTIgMCAzMi0xNS42IDMyLjEtMzIgLjA1LTcuNTUtMi42MS0xNS4yNy04LjYxLTIxLjcxeiI+PC9wYXRoPjwvc3ZnPg==';
+		const settingsControl =
+		'<div class="perfecty-push-settings-container">' +
+		'	 <div id="perfecty-push-settings-open">' +
+		'    <img src="' + svg + '" alt="Settings" width="30"/>' +
+		'  </div>' +
+		'  <div id="perfecty-push-settings-form">' +
+		' 	 <div>' + options.title + '</div>' +
+		'    <input type="checkbox" id="perfecty-push-settings-subscribed">' + options.subscribed + '</input>' +
+		'  </div>' +
+		'</div>';
+		document.body.insertAdjacentHTML('beforeend', settingsControl);
+	}
+
+	function showSettingsFormControl() {
+		const control = document.getElementById('perfecty-push-settings-form');
+		control.style.display = "block";
+	}
+
+	function hideSettingsFormControl() {
+		const control = document.getElementById('perfecty-push-settings-form');
+		control.style.display = "none";
+	}
+
+	function toggleSettingsFormControl() {
+		const control = document.getElementById('perfecty-push-settings-form');
+		const isDisplayed = control.style.display == "block";
+
+		control.style.display = isDisplayed ? "none" : "block";
+		if (isDisplayed) {
+			listenToOutideClick(control);
+		}
+	}
+
+	function listenToOutideClick (formControl) {
+		// from jquery: https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+		const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length );
+
+		const documentClickListener = (event) => {
+			if (!formControl.contains(event.target) && isVisible(formControl)) {
+				toggleSettingsFormControl();
+				removeClickListener();				
+			}
+		}
+		const removeClickListener = () => {
+			document.removeEventListener('click', documentClickListener);
+		}
+
+		document.addEventListener('click', documentClickListener);
 	}
 
 	// taken from https://github.com/mozilla/serviceworker-cookbook/blob/e912ace6e9566183e06a35ef28516af7bd1c53b2/tools.js
@@ -100,8 +148,9 @@
 	
 	async function perfectyStart(options) {
 		if (checkFeatures()) {
-			// Draw bell
+			// Draw dialog
 			drawDialogControl(options.dialogControl);
+			drawSettingsControl(options.settingsControl);
 
 	
 			// Notification permission
@@ -129,6 +178,19 @@
 				hideDialogControl();
 			}
 
+			document.getElementById('perfecty-push-settings-open').onclick = async () => {
+				toggleSettingsFormControl();
+			}
+
+			document.getElementById('perfecty-push-settings-subscribed').onchange = async (event) => {
+				const checked = event.target.checked;
+				if (checked == true && permission === 'default') {
+					showDialogControl();
+					// Subscribe
+				} else {
+					// Unsubscribe
+				}
+			}
 		} else {
 			console.log('Browser doesn\'t support notifications');
 		}
--- public/partials/perfecty-push-public-head.php
+++ public/partials/perfecty-push-public-head.php
@@ -2,6 +2,8 @@
   $dialog_title = isset($options['dialog_title']) ? $options['dialog_title'] : 'Do you want to receive notifications?';
   $dialog_submit = isset($options['dialog_submit']) ? $options['dialog_submit'] : 'Continue';
   $dialog_cancel = isset($options['dialog_cancel']) ? $options['dialog_cancel'] : 'Not now';
+  $settings_title = isset($options['settings_title']) ? $options['settings_title'] : 'Notifications preferences';
+  $settings_subscribed = isset($options['settings_subscribed']) ? $options['settings_subscribed'] : 'I want to receive notifications';
   $nonce = wp_create_nonce('wp_rest');
 ?>
 <script language="javascript">
@@ -11,6 +13,10 @@
       title: "<?php echo $dialog_title ?>",
       submit: "<?php echo $dialog_submit ?>",
       cancel: "<?php echo $dialog_cancel ?>"
+    },
+    settingsControl: {
+      title: "<?php echo $settings_title ?>",
+      subscribed: "<?php echo $settings_subscribed ?>"
     },
     siteUrl: "<?php echo get_home_url() ?>",
     serverUrl: "<?php echo PERFECTY_PUSH_SERVER_URL ?>",
