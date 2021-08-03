5540c9f70e4e3e646fb8c10ae1d84c0830cba89b
Rowinson Gallego <rwn.gallego@gmail.com>
2021-08-04 00:00:14 +0200

    Send logs to error_log() by default (#87)

--- lib/class-perfecty-push-lib-log.php
+++ lib/class-perfecty-push-lib-log.php
@@ -4,21 +4,24 @@
  * Logger
  */
 class Perfecty_Push_Lib_Log {
-	public const DEBUG   = 'debug';
-	public const INFO    = 'info';
-	public const WARNING = 'warning';
-	public const ERROR   = 'error';
+	public const DEBUG   = 0;
+	public const INFO    = 1;
+	public const WARNING = 2;
+	public const ERROR   = 3;
 
 	private static $writer;
 	private static $enabled = true;
+	private static $level   = self::ERROR;
 
 	/**
 	 * Sets the Log writer
 	 *
 	 * @param Perfecty_Push_Lib_Log_Writer $writer
+	 * @param int                          $level Error level
 	 */
-	public static function init( Perfecty_Push_Lib_Log_Writer $writer ) {
+	public static function init( Perfecty_Push_Lib_Log_Writer $writer, $level = self::ERROR ) {
 		self::$writer = $writer;
+		self::$level  = $level;
 	}
 
 	/**
@@ -38,48 +41,77 @@ class Perfecty_Push_Lib_Log {
 	/**
 	 * Logs a debug message
 	 *
-	 * @param string $message Message to log
+	 * @param string $message Message to log.
 	 */
 	public static function debug( $message ) {
-		if ( ! self::$enabled ) {
+		if ( ! self::$enabled || self::$level > self::DEBUG ) {
 			return;
 		}
-		self::$writer->write( self::DEBUG, $message );
+		self::$writer->write( self::level_string( self::DEBUG ), $message );
 	}
 
 	/**
 	 * Logs a info message
 	 *
-	 * @param string $message Message to log
+	 * @param string $message Message to log.
 	 */
 	public static function info( $message ) {
-		if ( ! self::$enabled ) {
+		if ( ! self::$enabled || self::$level > self::INFO ) {
 			return;
 		}
-		self::$writer->write( self::INFO, $message );
+		self::$writer->write( self::level_string( self::INFO ), $message );
 	}
 
 	/**
 	 * Logs a warning message
 	 *
-	 * @param string $message Message to log
+	 * @param string $message Message to log.
 	 */
 	public static function warning( $message ) {
-		if ( ! self::$enabled ) {
+		if ( ! self::$enabled || self::$level > self::WARNING ) {
 			return;
 		}
-		self::$writer->write( self::WARNING, $message );
+		self::$writer->write( self::level_string( self::WARNING ), $message );
 	}
 
 	/**
 	 * Logs an error message
 	 *
-	 * @param string $message Message to log
+	 * @param string $message Message to log.
 	 */
 	public static function error( $message ) {
-		if ( ! self::$enabled ) {
+		if ( ! self::$enabled || self::$level > self::ERROR ) {
 			return;
 		}
-		self::$writer->write( self::ERROR, $message );
+		self::$writer->write( self::level_string( self::ERROR ), $message );
+	}
+
+	/**
+	 * Get the level string
+	 *
+	 * @param $level int Level code.
+	 *
+	 * @return string
+	 * @since 1.3.3
+	 */
+	private static function level_string( $level ) {
+		$string = '';
+		switch ( $level ) {
+			case self::DEBUG:
+				$string = 'debug';
+				break;
+			case self::INFO:
+				$string = 'info';
+				break;
+			case self::WARNING:
+				$string = 'warning';
+				break;
+			case self::ERROR:
+				$string = 'error';
+				break;
+			default:
+				'';
+		}
+		return $string;
 	}
 }
--- lib/log/class-perfecty-push-lib-log-db.php
+++ lib/log/class-perfecty-push-lib-log-db.php
@@ -8,15 +8,10 @@ class Perfecty_Push_Lib_Log_Db implements Perfecty_Push_Lib_Log_Writer {
 	/**
 	 * Insert a log entry in the DB
 	 *
-	 * @param string $level Level code
-	 * @param string $message Message to log
+	 * @param string $level Level code.
+	 * @param string $message Message to log.
 	 */
 	public function write( $level, $message ) {
-		$db_version = get_option( 'perfecty_push_db_version' );
-		if ( $db_version >= 4 ) {
-			return Perfecty_Push_Lib_Db::insert_log( $level, $message );
-		} else {
-			error_log( strtoupper( $level ) . ' | ' . $message );
-		}
+		return Perfecty_Push_Lib_Db::insert_log( $level, $message );
 	}
 }
