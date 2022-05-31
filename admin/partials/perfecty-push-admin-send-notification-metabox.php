c5298e0ff3c0b0fb454b5c463bc887a4dc7348c8
Code2pixels <84022185+Code2pixels@users.noreply.github.com>
2022-05-30 20:50:04 -0400

    Updating ID and name of form input

--- admin/partials/perfecty-push-admin-send-notification-metabox.php
+++ admin/partials/perfecty-push-admin-send-notification-metabox.php
@@ -52,8 +52,8 @@
 			<input id="perfecty-push-send-notification-scheduled-date" name="perfecty-push-send-notification-scheduled-date" type="date" class="perfecty-push-notification-date" value="" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" placeholder="2021-05-26" disabled="disabled"/>
 			<input id="perfecty-push-send-notification-scheduled-time" name="perfecty-push-send-notification-scheduled-time" type="time" class="perfecty-push-notification-time" value="" pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}" placeholder="00:00:00" step=1 disabled="disabled"/>
 			<input id="perfecty-push-send-notification-timeoffset" name="perfecty-push-send-notification-timeoffset" type="hidden" value=""/>
-			<input id="perfecty-push-send-notification-recurring-notification" name="perfecty-push-send-notification-recurring-notification" type="checkbox" value="yes" />
-			<label for="perfecty-push-send-notification-recurring-notification"><?php printf( esc_html__( 'Recurring notification', 'perfecty-push-notifications' ) ); ?> <i><?php printf( esc_html__( '(Keeps recurring on the next day same time)', 'perfecty-push-notifications' ) ); ?></i></label>
+			<input id="perfecty-push-send-notification-recurring" name="perfecty-push-send-notification-recurring" type="checkbox" value="yes" />
+			<label for="perfecty-push-send-notification-recurring"><?php printf( esc_html__( 'Recurring notification', 'perfecty-push-notifications' ) ); ?> <i><?php printf( esc_html__( '(Keeps recurring on the next day same time)', 'perfecty-push-notifications' ) ); ?></i></label>
 		</p>
 	</div>
 </div>
