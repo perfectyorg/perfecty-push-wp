c264a6c03a89261ba0ce3c68e4622cbfe781e9d2
Rowinson Gallego <rwn.gallego@gmail.com>
2025-09-03 23:05:24 -0500

    Define constants that require translation after textdomain is loaded

--- includes/class-perfecty-push-i18n.php
+++ includes/class-perfecty-push-i18n.php
@@ -38,6 +38,59 @@ class Perfecty_Push_i18n {
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
+		
+		// Define text constants after textdomain is loaded
+		$this->define_text_constants();
+	}
+
+	/**
+	 * Define translatable text constants.
+	 *
+	 * @since    1.6.5
+	 */
+	public function define_text_constants() {
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_DIALOG_TITLE' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_DIALOG_TITLE',
+				esc_html__( 'Do you want to receive notifications?', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_DIALOG_CONTINUE' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_DIALOG_CONTINUE',
+				esc_html__( 'Continue', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_DIALOG_CANCEL' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_DIALOG_CANCEL',
+				esc_html__( 'Not now', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_SETTINGS_TITLE' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_SETTINGS_TITLE',
+				esc_html__( 'Notifications preferences', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_SETTINGS_OPT_IN' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_SETTINGS_OPT_IN',
+				esc_html__( 'I want to receive notifications', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_SETTINGS_UPDATE_ERROR' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_SETTINGS_UPDATE_ERROR',
+				esc_html__( 'Could not change the preference, try again', 'perfecty-push-notifications' )
+			);
+		}
+		if ( ! defined( 'PERFECTY_PUSH_OPTIONS_SETTINGS_WELCOME_MESSAGE' ) ) {
+			define(
+				'PERFECTY_PUSH_OPTIONS_SETTINGS_WELCOME_MESSAGE',
+				esc_html__( 'Congratulations, you\'re now subscribed!', 'perfecty-push-notifications' )
+			);
+		}
 	}
 
 
