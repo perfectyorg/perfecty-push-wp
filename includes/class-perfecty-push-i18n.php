25ddabd73388c87b089830488230d83782855d27
Rowinson Gallego <rwn.gallego@gmail.com>
2025-09-03 23:31:27 -0500

    Fix text domain in plugin header

--- includes/class-perfecty-push-i18n.php
+++ includes/class-perfecty-push-i18n.php
@@ -34,7 +34,7 @@ class Perfecty_Push_i18n {
 	 */
 	public function load_plugin_textdomain() {
 		load_plugin_textdomain(
-			'perfecty-push',
+			'perfecty-push-notifications',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
